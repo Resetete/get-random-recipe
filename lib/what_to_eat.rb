9aa2edc8c4d2ff63fb90e6128f43cae0c0962ba9
Theresa <tmannschatz@gmail.com>
2019-05-06 21:48:39 +0200

    Clean code

--- lib/what_to_eat.rb
+++ lib/what_to_eat.rb
@@ -1,18 +1,12 @@
 require 'net/http'
 require 'uri'
 
-#1. Bundle the gem:
-#  bundle gem what_should_i_eat --bin
-
 #1b. define API keys
 
-app_id = "26b201e9"
-app_key = "953e8d1f4d0fc6273d5471007e30f8f6"
+app_id = "" # enter your APP ID here
+app_key = "" # enter your API key here
 
-
-#2. make an HTTP GET request
-
-#User Input
+# Get User Input
 puts "What type of diet? (high-protein / balanced / low-fat / low-carb ) "
 diet = gets.chomp || ""
 
@@ -25,43 +19,14 @@ max_cal = gets.to_s.chomp
 
 uri = URI.parse("https://api.edamam.com/search?q=vegetarian&app_id=#{app_id}&app_key=#{app_key}&from=0&to=3&calories=50-#{max_cal}&diet=#{diet}&cuisine=#{cuisine}")
 response = Net::HTTP.get_response(uri)
-#puts response
-
-#puts response.code
-#puts response.body
 
 #3. Parse the returned text as JSON and save it into a variable
 require 'json'
-#TRAINING
-#hash = { ruby: "programming language" }
-#json = hash.to_json
-#puts json
 
-#json_course = "{\"course\":\"104\"}"
-#puts json_course
-#hash_course = JSON.parse(json_course)
-#puts hash_course
-
-# translate the json into a ruby hash (python: = dictionary)
-#recipes = JSON.parse(result)
-#puts recipes
 recipes = JSON.parse(response.body)
-#puts recipes
-
-#4. Pick out a recipe from the variable
-#puts recipes.class # Hash
-#puts recipes.keys # q, from, to, params
-#puts recipes['hits'].class # array
-#puts recipes['hits'].size
-#puts recipes['hits'].first.class # Hash
-#puts recipes['hits'].first.keys
-#puts recipes['hits'].first['recipe'].keys #hash
 
 name = recipes['hits'].first['recipe']['label']
 url = recipes['hits'].first['recipe']['url']
-#puts recipes.size # 7
-#puts recipes.first.class # array
-
 
 #5. Print that recipe to the screen
 puts recipes['hits'].first['recipe']['label']
